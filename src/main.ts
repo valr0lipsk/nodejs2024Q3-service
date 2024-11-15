301f327fc2a3853555c21f6d137ce7fe46524b8c
Valery Lipskaya <valery.lipskaya03@gmail.com>
2024-11-15 16:09:26 +0100

    fix: fix port from env

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +15,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(PORT);
 }
 bootstrap();
+console.log(`Server is running on port ${PORT}`);
